2359192cc4ca6be4274b144c58184ee584a1d52d
tech234a <46801700+tech234a@users.noreply.github.com>
2021-05-12 00:19:16 -0400

    Switch to websockets

--- irccloud.py
+++ irccloud.py
@@ -1,117 +1,26 @@
-# A Simple script to keep an irc cloud client always alive 
-__author__ = "S Vijaikumar"
-__email__  = "vijai@vijaikumar.in"
-__copyright__ = "Copyright (C) 2019 S Vijai Kumar"
-__license__ = "UNLICENSE"
-__version__ = "1.0"
-
 import requests
-import sys
-import traceback
-import logging
 from os import environ
 import heroku3
 from time import sleep
+import asyncio
+import ssl
+import websockets
 
-class irccloud:
-    """
-    This is a very simple class that takes an user's irc user name
-    and password as input and keeps the connection alive to emulate
-    the hearbeat produced due to real browser activity.
-    """
-    AuthenticationToken = ""
-    SessionId = ""
-    KeepAliveToken = ""
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 
-    def __init__(self, email, password, debug_mode=False):
-        self.email = email
-        self.password = password
-        self.debugging = debug_mode
-        logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG)
-        self.log = logging.getLogger(__name__)
+async def activate_socket():
+    uri = "wss://api.irccloud.com/"
+    async with websockets.connect(
+        uri, ssl=ssl_context
+    ) as websocket:
 
-    def get_authentication_token(self):
-        url = "https://www.irccloud.com/chat/auth-formtoken"
-        r = requests.post(url)
-        response = r.json()
-        if self.debugging:	
-            self.log.debug(response)
-        if response["success"]:
-            self.log.info("Successfully obtained authentication token.")
-            irccloud.AuthenticationToken = response["token"]
-        else:
-            self.log.error("Failed to obtain an authentication token.")
-            irccloud.AuthenticationToken = "AUTH_FAILURE"
+        await websocket.recv()
+            
+auth_formtoken = requests.post("https://www.irccloud.com/chat/auth-formtoken").json()["token"]
 
-    def get_session_id(self):
-        self.get_authentication_token()
-        if irccloud.AuthenticationToken == "AUTH_FAILURE":
-            return irccloud.AuthenticationToken
-        else:
-            login_url = "https://www.irccloud.com/chat/login"
-            headers = {
-                "content-type": "application/x-www-form-urlencoded",
-                "x-auth-formtoken":irccloud.AuthenticationToken
-            }
-            login_data = {
-                "email" : self.email,
-                "password" : self.password,
-                "token" : irccloud.AuthenticationToken
-            }
-            r = requests.post(login_url, data = login_data, headers = headers)
-            response = r.json()
-            if self.debugging:	
-                self.log.debug(response)
-            if response["success"]:
-                self.log.info("Successfully obtained a session id.")
-                irccloud.SessionId = response["session"]
-            else:
-                self.log.critical("Failed to obtain a session id.")
-                irccloud.SessionId = "SESSION_FAILURE"
+session_id = requests.post("https://www.irccloud.com/chat/login", data={"email": environ.get("IRCCLOUD_USERNAME"), "password": environ.get("IRCCLOUD_PASSWORD"), "token": auth_formtoken})
 
-    def keep_alive(self):
-        stream_url = "https://www.irccloud.com/chat/stream"
-        headers = {"Connection" : "keep-alive",
-                   "Accept-Encoding" : "gzip,deflate,sdch",
-                   "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-                   "Cookie": "session={0}".format(irccloud.SessionId),
-                   "Host":"www.irccloud.com"
-        }
-        r = requests.post(stream_url, headers = headers)
-        if self.debugging:	
-            self.log.debug(r.json())
-        if r.status_code == 200:
-            irccloud.KeepAliveToken = "KA_ALIVE"
-        else:
-            irccloud.KeepAliveToken = "KA_DEAD"
+asyncio.get_event_loop().run_until_complete(activate_socket())
 
-    def runner(self):
-        self.get_session_id()
-        self.log.debug("   IRC Cloud Keep alive Func   ")
-        if irccloud.SessionId == "SESSION_FAILURE":
-            self.log.critical("Couldn't get a session initialized. Quitting... :(")
-            sys.exit(0)
-        else:
-            self.keep_alive()
-            if irccloud.KeepAliveToken == "KA_ALIVE":
-                self.log.info("IRC Cloud Session is Kept alive.")
-            else:
-                self.log.error("IRC Cloud Session could not be Kept alive.")
-            heroku3.from_key(environ['heroku-key']).apps()[environ['heroku-app-name']].scale_formation_process('worker', 0)
-            sleep(1)
-            sys.exit(0)
-
-
-if __name__ == "__main__":
-    try:
-        email = environ.get("IRCCLOUD_USERNAME")
-        password = environ.get("IRCCLOUD_PASSWORD")
-        debug_mode = True	
-        irc = irccloud(email, password, debug_mode)
-        irc.runner()
-    except KeyboardInterrupt:
-        self.log.debug("Shutdown requested. Exiting script. Thank you :)")
-        sys.exit(0)
-    except Exception:
-        traceback.print_exc(file=sys.stdout)
-        sys.exit(0)
+heroku3.from_key(environ['heroku-key']).apps()[environ['heroku-app-name']].scale_formation_process('worker', 0)
+sleep(1)
